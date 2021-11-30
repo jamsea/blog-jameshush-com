778bbbaec8e1f173c6e3033118f935d580062629
James Hush <hello@jameshush.com>
2021-11-30 17:34:03 +0800

    feat: add active campaign tracking

---
+++ gatsby-ssr.js
@@ -0,0 +1,25 @@
+const React = require("react")
+
+// If anyone knows of a better way to do this, please make a pull request 🤷
+function renderActiveCampaignTracking() {
+  if (process.env.NODE_ENV !== `production` && process.env.NODE_ENV !== `test`)
+    return
+
+  return `(function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
+    vgo('setAccount', '651138045');
+    vgo('setTrackByDefault', true);
+
+    vgo('process');`
+}
+
+const postBodyComponents = [
+  <script
+    type="text/javascript"
+    key={`gatsby-plugin-active-campaign`}
+    dangerouslySetInnerHTML={{ __html: renderActiveCampaignTracking() }}
+  />,
+]
+
+exports.onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents(postBodyComponents)
+}
