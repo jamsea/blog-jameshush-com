9ad801ce84e9c4199a116f9ae370d2da39c1ba68
James Hush <hello@jameshush.com>
2021-12-15 19:18:27 +0800

    feat: add style to bug funnel page

--- src/pages/bug-funnel.tsx
+++ src/pages/bug-funnel.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import Layout from "../components/layout"
 
 const ActiveCampaignForm = () => {
   const [formSubmitted, setFormSubmitted] = useState(false)
@@ -55,10 +56,37 @@ const ActiveCampaignForm = () => {
   )
 }
 
-export default function Component() {
+type BugFunnelProps = {
+  location: Location
+  title?: string
+  children: React.ReactNode
+}
+
+const BugFunnel = ({ location }: BugFunnelProps) => {
   return (
-    <div>
+    <Layout location={location} title={"bug funnel"}>
+      <h1>Want to ship more high-quality features faster?</h1>
       <ActiveCampaignForm />
-    </div>
+      <p>
+        "We don't deploy on Fridays." Have you heard this before? I used to feel
+        the same way. Until we had to push an update to Superbowl.com during the
+        Super Bowl.
+      </p>
+
+      <p>
+        Since then, I've taken what I've learned and applied it at multiple
+        companies. I've taken teams from deploying for four hours with three
+        people, to deploying three times a day in no time. With less outages.
+      </p>
+
+      <p>
+        Sign up today and I'll send you an email once a week with each step I
+        apply to every company I help. I believe we all have the time today to
+        do proactive work as engineers, we just need to figure out what to focus
+        on first.
+      </p>
+    </Layout>
   )
 }
+
+export default BugFunnel
