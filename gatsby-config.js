02c72af6301f015aa1120a80d70dfb3dfaa0c99b
James Hush <hello@jameshush.com>
2021-12-16 22:28:03 +0800

    feat: add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,12 +66,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `G-XNZS36KWBN`,
+      },
+    },
     {
       resolve: `gatsby-plugin-feed`,
       options: {
