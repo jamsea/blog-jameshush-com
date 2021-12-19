69b414c625ea623be9320baffcb001b7f2aa23b4
James Hush <hello@jameshush.com>
2021-12-19 19:03:06 +0800

    feat: update google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,9 +67,17 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: `G-XNZS36KWBN`,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          `G-XNZS36KWBN`, // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+        },
       },
     },
     {
