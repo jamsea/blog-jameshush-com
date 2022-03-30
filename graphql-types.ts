9b9385f8b4f6374fdefd26ce6af681095766bffb
James Hush <hello@jameshush.com>
2022-03-30 09:36:10 +0800

    fix: add exenv to check if its on the server

--- graphql-types.ts
+++ graphql-types.ts
@@ -247,8 +247,6 @@ export type DirectoryCtimeArgs = {
 export type Site = Node & {
   buildTime?: Maybe<Scalars['Date']>;
   siteMetadata?: Maybe<SiteSiteMetadata>;
-  port?: Maybe<Scalars['Int']>;
-  host?: Maybe<Scalars['String']>;
   polyfill?: Maybe<Scalars['Boolean']>;
   pathPrefix?: Maybe<Scalars['String']>;
   jsxRuntime?: Maybe<Scalars['String']>;
@@ -822,8 +820,6 @@ export type QueryAllDirectoryArgs = {
 export type QuerySiteArgs = {
   buildTime?: InputMaybe<DateQueryOperatorInput>;
   siteMetadata?: InputMaybe<SiteSiteMetadataFilterInput>;
-  port?: InputMaybe<IntQueryOperatorInput>;
-  host?: InputMaybe<StringQueryOperatorInput>;
   polyfill?: InputMaybe<BooleanQueryOperatorInput>;
   pathPrefix?: InputMaybe<StringQueryOperatorInput>;
   jsxRuntime?: InputMaybe<StringQueryOperatorInput>;
@@ -2010,8 +2006,6 @@ export type SiteFieldsEnum =
   | 'siteMetadata___author___name'
   | 'siteMetadata___author___summary'
   | 'siteMetadata___siteUrl'
-  | 'port'
-  | 'host'
   | 'polyfill'
   | 'pathPrefix'
   | 'jsxRuntime'
@@ -2147,8 +2141,6 @@ export type SiteGroupConnectionGroupArgs = {
 export type SiteFilterInput = {
   buildTime?: InputMaybe<DateQueryOperatorInput>;
   siteMetadata?: InputMaybe<SiteSiteMetadataFilterInput>;
-  port?: InputMaybe<IntQueryOperatorInput>;
-  host?: InputMaybe<StringQueryOperatorInput>;
   polyfill?: InputMaybe<BooleanQueryOperatorInput>;
   pathPrefix?: InputMaybe<StringQueryOperatorInput>;
   jsxRuntime?: InputMaybe<StringQueryOperatorInput>;
--- src/pages/daily.tsx
+++ src/pages/daily.tsx
@@ -1,18 +1,21 @@
 import * as React from "react"
 
 import DailyIframe from "@daily-co/daily-js"
+import ExecutionEnvironment from "exenv"
 
 const DailyPage: React.FC = () => {
-  const callFrame = DailyIframe.createFrame({
-    showLeaveButton: true,
-    iframeStyle: {
-      position: "fixed",
-      width: "100%",
-      height: "100%",
-    },
-  })
+  if (ExecutionEnvironment.canUseDOM) {
+    const callFrame = DailyIframe.createFrame({
+      showLeaveButton: true,
+      iframeStyle: {
+        position: "fixed",
+        width: "100%",
+        height: "100%",
+      },
+    })
 
-  callFrame.join({ url: "https://jameshush.daily.co/jamestest" })
+    callFrame.join({ url: "https://jameshush.daily.co/jamestest" })
+  }
 
   return (
     <>
